ace9c5d756c3ffb944a16acf4f6ac438fcbd24e7
Decagon <uchecharles223@gmail.com>
2023-01-10 14:51:48 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import HomePages from "./home/HomePage";
+import Footer from "./components/footer/Footer";
+import SinglePage from "./components/watch/singlePage"
 
 const App = () => {
   return (
@@ -11,7 +13,9 @@ const App = () => {
         <Header />
         <Routes>
           <Route path='/' element={<HomePages/>} />
+          <Route path='/singlepage/:id' element={<SinglePage/>} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
